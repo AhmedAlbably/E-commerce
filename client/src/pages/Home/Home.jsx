24809e7fcc915432237a2d90aba6f40f0c1f0eea
AhmedAlbably <ahmedalbably777@gmail.com>
2025-02-24 09:47:02 +0200

    there are any changes

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
 import "./Home.scss";
+import Aside from "../../components/Home/Aside/Aside";
 
 function Home() {
   const {email}= useSelector((state) => state.resetDataPass)
   console.log(email)
-  return <div>Home</div>;
+  return <div>
+    <Aside/>
+  </div>;
 }
 
 export default Home;
